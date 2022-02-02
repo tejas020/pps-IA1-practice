0f58c7507a9f6aee556ff1af155c7d42fd01c301
tejas020 <94588241+tejas020@users.noreply.github.com>
2022-02-02 10:39:43 +0000

    nil

--- p6original.c
+++ p6original.c
@@ -1,31 +1,67 @@
 #include<stdio.h>
-int input_n()
-{
-  int x;
-  printf("enter no. of terms:");
-  scanf ("%d",&x);
-  return x;
-}
 
-int sum_n(int n)
+void input_two_strings(char *a, char *b)
 {
-  int i,sum=0;
-  for(i=0;i<=n;i++)
+  printf("Enter the word: ");
+  scanf("%s", a);
+  printf("Enter the word: ");
+  scanf("%s", b);
+}
+int cmp(char *a, char *b)
+{
+  int i=0, result;
+  while(a[i] != '\0' && b[i] != '\0')
   {
-    sum=sum+i;
+  if (a[i] > b[i])
+  {
+  result = -1;
+  break;
+  }
+  else if(b[i] > a[i])
+  {
+  result = 1;
+  break;
+  }
+  else
+  {
+  i++;
+  }
+  }
+  if(a[i] == '\0')
+  {
+  result = 1;
+  }
+  else if(b[i] == '\0')
+  {
+  result = -1;
+  }
+  if (a[i] == '\0' && b[i] == '\0')
+  {
+  result =0;
+  }
+  return result;
 }
-  return sum;
-}
-
-void output(int n, int sum)
+void output(char*a, char*b, int ans)
 {
-  printf("your result of %d is %d",n,sum);
+  if(ans == -1)
+  {
+  printf("%s is greater than %s \n", a, b);
+  }
+  else if(ans == 1)
+  {
+  printf("%s is greater than %s \n", b, a);
+  }
+  else
+  {
+  printf("%s is equal to %s\n", a, b);
+  }
 }
 int main()
 {
-  int n,f;
-  n=input_n();
-  f=sum_n(n);
-  output(n,f);
+  char a[20], b[20];
+  int ans;
+  input_two_strings(a, b);
+  ans = cmp(a, b);
+  output(a, b, ans);
   return 0;
 }
