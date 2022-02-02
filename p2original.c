a0727b7cd1690ddae80355cc243b9edc469c02fd
tejas020 <94588241+tejas020@users.noreply.github.com>
2022-02-02 10:30:01 +0000

    nil

--- p2original.c
+++ p2original.c
@@ -1,34 +1,20 @@
-#include <stdio.h>
-
-int input()
-{
-  int x;
-  printf("enter the number");
-  scanf("%d",&x);
-  return x;
-}
-
-int add(int a,int b)
-{
-  int c;
-  c=a+b;
-  return c;
-}
-
-
-
-int output(int a, int b,int c)
-{
-  printf("your result of a and b is %d+%d=%d",a,b,c);
-
-}
-
+#include<stdio.h>
 int main()
 {
   int a,b,c;
-  a=input();
-  b=input();
-  c=add(a,b);
-  output(a,b,c);
+  printf("enter 3 numbers\n");
+  scanf("%d %d %d",&a,&b,&c);
+  if((a>b)&&(a>c))
+  {
+  printf("%d is the largest number\n",a);
+  }
+  else if((b>a)&&(b>c))
+  {
+  printf("%d is the largest number\n",b);
+  }
+  else
+  {
+  printf("%d is the largest number",c);
+  }
   return 0;
-}
+} 
